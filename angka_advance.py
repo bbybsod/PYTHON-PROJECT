7305db7f81eb179da534c381904ed6bd419cf29e
rasya_a <145573157+bbybsod@users.noreply.github.com>
2025-01-18 11:22:48 +0800

    Add files via upload

---
+++ angka_advance.py
@@ -0,0 +1,8 @@
+data = int(input("masukan data :"))
+print("data ini adalah =", data, ",type =", type(data))
+data_bool = bool(data)
+data_float = float(data)
+data_str = str(data)
+print("data ini adalah =", data_bool, ",type =", type(data_bool))
+print("data ini adalah =", data_float, ",type =", type(data_float))
+print("data ini adalah =", data_str, ",type =", type(data_str))
---
+++ kalkulator perkalian.py
@@ -0,0 +1,5 @@
+a = int(input("masukan angka pertama :"))
+b = int(input("masukan angka kedua :"))
+
+a * b 
+print("hasil =", a * b)
