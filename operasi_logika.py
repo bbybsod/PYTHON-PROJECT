cd4a55a272a6916970bc829575f85ee43bd3b8dd
rasya_a <145573157+bbybsod@users.noreply.github.com>
2025-01-19 17:03:54 +0800

    Add files via upload

---
+++ operasi_logika.py
@@ -0,0 +1,61 @@
+print('OPERASI LOGIKA OR,NOT,AND,XOR')
+print('===========BY RASYA==========')
+print('============NOT==============')
+a  = True 
+b  = not a 
+print('data a =' , a)
+print('data c =', b)
+
+print('==========OR==========')
+a = True
+b = False
+c = a or b 
+print(a, 'or' , b ,'=', c, )
+a = True
+b = True
+c = a or b
+print(a, 'or', b, '=', c)
+a = False
+b = True
+c = a or b
+print(a, 'or', b, '=', c)
+a = False
+b = False
+c = a or b
+print(a, 'or', b, '=', c)
+
+print('==========AND==========')
+a = True
+b = False
+c = a and b 
+print(a, 'and' , b ,'=', c, )
+a = True
+b = True
+c = a and b
+print(a, 'and', b, '=', c)
+a = False
+b = True
+c = a and b
+print(a, 'and', b, '=', c)
+a = False
+b = False
+c = a and b
+print(a, 'and', b, '=', c)
+
+print('==========XOR==========')
+a = True
+b = False
+c = a ^ b 
+print(a, 'xor' , b ,'=', c, )
+a = True
+b = True
+c = a ^ b
+print(a, 'xor', b, '=', c)
+a = False
+b = True
+c = a ^ b
+print(a, 'xor', b, '=', c)
+a = False
+b = False
+c = a ^ b
+print(a, 'xor', b, '=', c)
