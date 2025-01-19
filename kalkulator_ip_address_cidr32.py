d62a3065de5e4a5d7cfe92db197f05fbc5744590
rasya_a <145573157+bbybsod@users.noreply.github.com>
2025-01-19 17:52:22 +0800

    update jumlah ip

--- kalkulator_ip_address_cidr32.py
+++ kalkulator_ip_address_cidr32.py
@@ -15,6 +15,7 @@ print("jumlah ip =", jumlah_ip, )
 ip_host = jumlah_ip - 2
 print("jumlah ip = host =", ip_host, )
 
+
 range_ip = (d/jumlah_ip)*jumlah_ip 
 print("range ip pertama =", range_ip, )
 range_ip2 = (range_ip + jumlah_ip) - 1 
