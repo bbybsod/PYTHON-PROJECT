3e64ecc54a4a433f5dcdd6279972b0e62eb31e5b
rasya_a <145573157+bbybsod@users.noreply.github.com>
2025-01-20 14:59:24 +0800

    update

--- komparasiLogika.py
+++ komparasiLogika.py
@@ -1,5 +1,9 @@
 print('==========KOMPARASI LOGIKA==========')
+print('================RASYA===============')
+print('=====kurang dari=====')
 user1 = int(input('masukan angka :'))
 angka1 = user1 
 hasil = (user1 < 3 )
 print('angka adalah', hasil)
+
+user2 = int(input('masukan angka '))
