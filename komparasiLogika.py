270adf77f7384a30a27199724788bb96d410c445
rasya_a <145573157+bbybsod@users.noreply.github.com>
2025-01-20 15:05:03 +0800

    update

--- komparasiLogika.py
+++ komparasiLogika.py
@@ -5,7 +5,7 @@ user1 = int(input('masukan angka :'))
 angka1 = user1 
 hasil = (user1 < 3 )
 print('angka adalah', hasil)
-
+print('=====lebih dari=====')
 user2 = int(input('masukan angka '))
 angka2 = user2
 hasil = (user2 > 3)
