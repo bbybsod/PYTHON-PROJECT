25688337ac88ae601f1d3ab66e1a9b1fbc4db8bb
sean myxzu <145573157+bbybsod@users.noreply.github.com>
2025-02-07 16:08:04 +0800

    update

---
+++ calc.py
@@ -0,0 +1,18 @@
+oat(input('masukan angka pertama : '))
+operator = input('masukan operator "+,/,-,x": ')
+data_2 = float(input('masukan angka kedua : '))
+
+if operator == '+':
+    hasil = data_1 + data_2
+    print(f'hasilnya adalah {hasil}')
+elif operator == 'x':
+    hasil = data_1 * data_2
+    print(f'hasilnya adalah {hasil}')
+elif operator == '-': 
+    hasil = data_1 - data_2
+    print(f'hasilnya adalah {hasil}')
+elif operator == '/': 
+    hasil = data_1 / data_2
+    print(f'hasilnya adalah {hasil}')
+else: 
+    print('invalid command')
