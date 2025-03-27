a273c8144d968296fdda6ef49447bea4187c1cd7
sean myxzu <145573157+bbybsod@users.noreply.github.com>
2025-03-27 13:15:16 +0800

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,21 @@
+print(10*'=')
+print('data list looping')
+angka_datalist = [1,2,3,4,5,6,7,8,9,10]
+for rasya in angka_datalist:
+    print(f'rasya ada di -> {rasya}')
+
+print('akhir dari program data list ')
+
+print(10*'=')
+print('data range')
+angka_datarange = range(10)
+for anjay in angka_datarange:
+    print(f"anjay ada di -> {anjay}")
+
+print('akhir dari program data range /n')
+
+print(10*"loop string")
+print('data string looping')
+looping_string = "rasya ganteng"
+for i in looping_string:
+    print(i)
